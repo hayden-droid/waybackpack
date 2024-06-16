baad7efa817e37aa03c001d8122b42b05781b1cb
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2024-06-16 20:52:05 +0100

    Delete waybackpack directory

--- waybackpack/__init__.py
+++
@@ -1,7 +0,0 @@
-from .asset import Asset
-from .cdx import search
-from .pack import Pack
-from .session import Session
-from .version import __version__
-
-__all__ = ["Asset", "search", "Pack", "Session", "__version__"]
--- waybackpack/settings.py
+++
@@ -1,2 +0,0 @@
-DEFAULT_ROOT = "https://web.archive.org"
-DEFAULT_USER_AGENT = "waybackpack"
